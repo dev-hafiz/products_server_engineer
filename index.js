8e57e4380e383079e1b8c07f8cd99857a77b2bea
dev-hafiz <johnstarhafiz@gmail.com>
2024-12-04 11:51:48 +0600

    This server will provide a RESTful API endpoint to expose product information

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.SECRET_USER_NAME}:${process.env.SECRET_KEY_PASSWORD}@cluster0.luy9u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    //Collection and Database
+    const productsCollection = client.db("Products_DB").collection("products");
+
+    //Get Method (default all)
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //Get Method (specific one by id)
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Send successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//MiddleWare
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Products server is running!");
+});
+
+app.listen(port, () => {
+  console.log(`Products server listening on port ${port}`);
+});
